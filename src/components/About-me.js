9896c46f71c3e9732e68c1055e2bbc24058fe053
agu0306 <agu0306@gmail.com>
2025-12-17 20:01:18 -0300

    EDIT About me section

--- src/components/About-me.js
+++ src/components/About-me.js
@@ -4,17 +4,9 @@ function Intro() {
     return (
         <>
             <h3>Sobre mi</h3>
-            <p className='about-me'> Mi nombre es Agustina, tengo 33 años y estoy en búsqueda de mi primer empleo en TI.
-                Comencé a interesarme en la programación durante la pandemia cuando inicié un curso de Analista Programador en &nbsp;
-                <a href="https://ctc.edu.uy/sede/colonia/" target='blank' style={{color: 'white'}}>Instituto CTC Colonia</a> mientras trabajaba como auxiliar contable en un Estudio. Durante el transcurso de este tiempo
-                aprendí diferentes tecnologías y lenguajes, así como la incorporación de herramientas eficaces para la programación
-                en sí y para el desarrollo ágil de proyectos orientados al trabajo en equipo, tanto front como back-end.
-                Actualmente estoy enfocada en desarrollo front-end y me encuentro trabajando en un proyecto personal para
-                colaborar con el Estudio donde trabajaba, que tiene
-                como objetivo automatizar y optimizar tareas contables, espero en un futuro aumentar el alcance de este proyecto y
-                colaborar con mas empresas.
-                Me interesa formar parte de equipos y proyectos que me desafíen y pueda aprender de ellos, ganando experiencia,
-                colaborando y realizando aportes.</p>
+            <p className='about-me'>  &nbsp;
+                 Estudiante avanzada de analista programador en <a href="https://ctc.edu.uy/sede/colonia/" target='blank' style={{ color: 'white' }}>Instituto CTC Colonia</a> Mi experiencia está basada en proyectos que desarrollé por mi cuenta y como parte de mi formación.
+                Actualmente trabajo como administrativa en una empresa y también colaboro con el desarrollo de su sitio web. Además de forma paralela, me encuentro deseñando un software adaptado a las necesidades de la misma, con el fin de automatizar procesos, optimizar el trabajo y disminuir errores. Mi objetivo es incorporarme a un equipo de trabajo donde pueda seguir creciendo y dedicarme por completo a la programación.</p>
         </>
     )
 }
